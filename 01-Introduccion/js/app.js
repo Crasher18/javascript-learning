812277632df95627736ecc22ee07ac0ca945051f
Yeferson <cortestorresyeferson@gmail.com>
2024-06-08 17:14:00 -0500

    fix bug

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -2,4 +2,4 @@ const nombre = prompt("cual es tu nombre?");
 
 document.querySelector(
   ".contenido"
-).innerHTML = `${nombre} quiere decirte que sera el mejor aprendiendo`;
+).innerHTML = `${nombre} quiere decirte que sera el mejor aprendiendo programacion`;
